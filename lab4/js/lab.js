e041aaec7cee61f636eefebf3f70b637b7f8167c
Gabrielar1904 <129999853+Gabrielar1904@users.noreply.github.com>
2023-04-24 16:51:37 -0700

    made changes to javascript and html

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,17 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Connect 4
+// An Implementation of the game
+// Author: Gabriela Rioja grioja@ucsc.edu
+// Date: April 24, 2023
+
+// Print the board before game starts
+// Accept user input when choosing oppenent color
+// Accept users decision when placing peg on board
+// Loop through each row/column 
+  // Loop through each slot
+    //If user 1 pieces = horizontal, diagonal or vertical...
+      // Print user 1 wins
+    //Else if user 2 pieces = horizontal, diagonal or vertical...
+      // Print user 2 wins
 
 // Constants
 
