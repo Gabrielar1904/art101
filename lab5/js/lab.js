20ec245adbccb17fcaaeee21f41af0ce36325b5f
Gabrielar1904 <129999853+Gabrielar1904@users.noreply.github.com>
2023-04-26 14:16:01 -0700

    made changes to javascript

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
 
-// Constants
+// Author: Gabriela Rioja grioja@ucsc.edu
+// Date: April 26, 2023
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//
+make = "Volkswagen";
+model = "Jetta";
+color = "White";
+year = 2012;
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//
+age = 2023 - year;
 
-// let's get this party started
-main();
+//
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
+
+
